d41ef447041e1dedaf53d1aaabb171ff4a146a1b
manabu-s <manabu-s@iij.ad.jp>
2019-04-18 13:11:03 +0900

    update send udp query

--- proxy.go
+++ proxy.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"encoding/base64"
-	"fmt"
 	"net"
 	"strconv"
 	"time"
@@ -89,81 +88,38 @@ func (p *Proxy) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
 		"func": "HandleFastHTTP",
 		"msg":  dnsMsg,
 	}).Debug("start proxy query")
-	conn, err := net.Dial("udp", p.host)
+	c := new(dns.Client)
+	c.Net = "udp"
+	r, _, err := c.Exchange(&msg, p.host)
+
 	if err != nil {
 		log.WithFields(log.Fields{
-			"func":  "HandleFastHTTP",
-			"msg":   dnsMsg,
-			"Error": err,
-		}).Debug("failed to open tcp socket.")
-		ctx.Error("failed to open tcp socket.", fasthttp.StatusInternalServerError)
-		return
-	}
-	defer conn.Close()
-	log.WithFields(log.Fields{
-		"func": "HandleFastHTTP",
-	}).Debug("connection success")
-
-	if _, err := conn.Write(dnsMsg); err != nil {
-		log.WithFields(log.Fields{
 			"func": "HandleFastHTTP",
-		}).Debug("failed to write dns query")
+		}).Debug("failed to dns query")
 		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
+		return
 	}
 	log.WithFields(log.Fields{
 		"func": "HandleFastHTTP",
 	}).Debug("Success to send dns message")
 
 	ctx.Response.Header.SetContentTypeBytes(strDnsContentType)
-
-	recvBuf := []byte{}
-	var size int
-	for {
-		l := make([]byte, bufferSize)
-		n, err := conn.Read(l)
-		recvBuf = append(recvBuf, l[0:n]...)
-		size += n
-		if n != bufferSize {
-			break
-		}
-		if err != nil {
-			log.WithFields(log.Fields{
-				"func": "HandleFastHTTP",
-			}).Debug("failt to read message")
-		}
-	}
-	log.WithFields(log.Fields{
-		"func": "HandleFastHTTP",
-	}).Debug("Success to receive dns message")
+	recvBuf, err := r.Pack()
 
 	if err != nil {
 		log.WithFields(log.Fields{
 			"func": "HandleFastHTTP",
-		}).Debug("failed to write dns query")
+		}).Debug("pack query")
 		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
 		return
 	}
 	ctx.Response.SetBody(recvBuf)
 
 	// parse ttl
-	msg = dns.Msg{}
-	err = msg.Unpack(recvBuf)
-	if err != nil {
-		data := ""
-		for _, v := range recvBuf {
-			data += fmt.Sprintf("%x", v)
-		}
-		log.WithFields(log.Fields{
-			"func": "HandleFastHTTP",
-			"data": data,
-		}).Debug("failed to parse query")
-		ctx.Error("failed to parse query.", fasthttp.StatusInternalServerError)
-		return
-	}
-	if len(msg.Answer) > 0 {
-		ctx.Response.Header.SetBytesK(strDnsCacheControl, strconv.FormatUint(uint64(msg.Answer[0].Header().Ttl), 10))
-	} else if len(msg.Ns) > 0 {
-		ctx.Response.Header.SetBytesK(strDnsCacheControl, strconv.FormatUint(uint64(msg.Ns[0].Header().Ttl), 10))
+	if len(r.Answer) > 0 {
+		ctx.Response.Header.SetBytesK(strDnsCacheControl, strconv.FormatUint(uint64(r.Answer[0].Header().Ttl), 10))
+	} else if len(r.Ns) > 0 {
+		ctx.Response.Header.SetBytesK(strDnsCacheControl, strconv.FormatUint(uint64(r.Ns[0].Header().Ttl), 10))
 	}
 	if p.dnstap {
 		tapMsg := g.ClientResponse(recvBuf)
